f50b92bccd1daa62e594cb01c01bf2797860fc6f
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-06-26 15:36:47 -0400

    Most replayed

--- base/views.py
+++ base/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn python!'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend develop'},
-# ]
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend develop'},
+]
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
