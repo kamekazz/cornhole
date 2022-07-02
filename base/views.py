a8e5483a0270249a883c720eaff5be6c2278c33f
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-02 14:54:26 -0400

    chat room messages

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -80,12 +80,25 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    print(pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    context = {
+        'room': room,
+        'room_messages': room_messages,
+        'participants': participants
+    }
     return render(request, 'pages/room.html', context)
 
 
-@login_required(login_url='login')
+@ login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -97,7 +110,7 @@ def createRoom(request):
     return render(request, 'pages/room_form.html', context)
 
 
-@login_required(login_url='login')
+@ login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -114,14 +127,27 @@ def updateRoom(request, pk):
     return render(request, 'pages/room_form.html', context)
 
 
-@login_required(login_url='login')
+@ login_required(login_url='login')
 def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
 
     if request.user != room.host:
         return HttpResponse('Your are not allowed here!!')
 
-    room = Room.objects.get(id=pk)
     if request.method == 'POST':
         room.delete()
         return redirect('home')
     return render(request, 'pages/delete.html', {'obj': room})
+
+
+@ login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'pages/delete.html', {'obj': message})
