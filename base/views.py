2060a513eaa1a0823810fb90292e475bad3c83cc
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-04 12:57:55 -0400

    fix components

--- base/views.py
+++ base/views.py
@@ -109,16 +109,20 @@ def room(request, pk):
 @ login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'pages/room_form.html', context)
 
 
